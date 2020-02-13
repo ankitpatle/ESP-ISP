255dcae98b3fbab878996f34d32d8260257c26c2
Ankit Patle <ankitpatle1996@gmail.com>
2020-02-13 23:23:51 +0530

    Update ESP_ISP.ino

--- ESP_ISP/ESP_ISP.ino
+++ ESP_ISP/ESP_ISP.ino
@@ -1,3 +1,20 @@
+/******************************************************************************************************************************************************************
+ESP-ISP (ESP32 based ISP for Atmel 8051 controllers)
+Copyright (C) 2020  Ankit Patle
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************************************************************************************************************/
 #include <WiFi.h>
 #include <ESPmDNS.h>
 #include <AsyncTCP.h>
